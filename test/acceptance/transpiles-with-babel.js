8e6e970d26c20a825f51f14c2258dfd4684cf1c3
Dave Wasmer <davewasmer@gmail.com>
2017-02-19 23:18:47 -0700

    add basic acceptance test

---
+++ test/acceptance/transpiles-with-babel.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import * as fs from 'fs';
+import * as path from 'path';
+import { CommandAcceptanceTest } from 'denali';
+
+test('transpiles source with babel', async (t) => {
+  let build = new CommandAcceptanceTest('build', { name: 'build-command' });
+  await build.run();
+  let builtFile = path.join(build.dir, 'dist', 'app', 'application.js');
+
+  t.true(fs.existsSync(builtFile), 'built files were created');
+  t.false(fs.readFileSync(builtFile, 'utf-8').includes('import'), 'built files were transpiled');
+});
