af761599aef77bbb32d4aeb9245e7b2b2eb606e6
Dave Wasmer <davewasmer@gmail.com>
2017-02-19 23:19:18 -0700

    add default blueprint that adds a .babelrc

---
+++ blueprints/denali-babel/index.js
@@ -0,0 +1,8 @@
+import { Blueprint } from 'denali-cli';
+
+export default class DenaliBabelBlueprint extends Blueprint {
+
+  static blueprintName = 'denali-babel';
+  static description = 'Generate the default .babelrc';
+
+}
